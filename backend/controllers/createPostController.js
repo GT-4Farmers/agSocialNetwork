8a804d96ffa9a553f3db7f40458e46c839793423
soobak <jnam08@gmail.com>
2021-10-19 17:20:22 -0400

    Fix SQL syntax for inserting to Images

--- backend/controllers/createPostController.js
+++ backend/controllers/createPostController.js
@@ -4,10 +4,10 @@ exports.createPostController = (req, res) => {
     const uuid = require("uuid")
     
     let content = req.body.content;
-    let image_name = req.body.image_name
-    let image_loc = req.body.image_loc
+    let image_name = req.body.image_name;
+    let image_loc = req.body.image_loc;
     let user = req.session.userID;
-    let post_id = uuid.v4()
+    let post_id = uuid.v4();
 
     var sql = `INSERT INTO Posts (postID, createdBy, content) VALUES (?, ?, ?)`;
     var input = [post_id, user, content];
@@ -27,20 +27,20 @@ exports.createPostController = (req, res) => {
                 })
                 return;
             } else {
-                db.query("INSERT INTO Images (File_reference, filename, postID) VALUES ?, ?, ?", [image_loc, image_name, post_id], (err, data, fields) => {
-                            if (err) {
-                                res.json({
-                                    success: false,
-                                    msg: err
-                                })
-                                return;
-                            } else {
-                                res.json({
-                                    success: true,
-                                    msg:'Successfully created post with an image.'
-                                })
-                                return;
-                            }
+                db.query("INSERT INTO Images (File_reference, Filename, postID) VALUES (?, ?, ?)", [image_loc, image_name, post_id], (err, data, fields) => { 
+                    if (err) {
+                        res.json({
+                            success: false,
+                            msg: err
+                        })
+                        return;
+                    } else {
+                        res.json({
+                            success: true,
+                            msg:'Successfully created post with an image.'
+                        })
+                        return;
+                    }
                             
                 })
             }
