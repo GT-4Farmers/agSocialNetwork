93f90b0708d6984f4ff0e0435abdde7f483d51ac
dillon993 <79716721+dillon993@users.noreply.github.com>
2021-11-11 14:13:55 -0500

    Fix timestamp sorting

--- backend/controllers/getPhotosController.js
+++ backend/controllers/getPhotosController.js
@@ -1,7 +1,7 @@
 exports.getPhotosController = (req, res) => {
     const db = require("../server");
     let uuid = req.body.profileRoute;
-    db.query("SELECT File_reference FROM Media WHERE uuid = ? ORDER BY createdAt", uuid, (err, data) => {
+    db.query("SELECT File_reference FROM Media WHERE uuid = ? ORDER BY createdAt DESC", uuid, (err, data) => {
         if (data[0]) {
             let photos = [];
             for (const key in data) {
